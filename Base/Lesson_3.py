1e13d65916cee4f853ca5ac4cf0df5da5c0ad3b4
agrec <agrechushnikov@gmail.com>
2022-02-04 23:20:11 +0300

    Добавлен 3-й урок

---
+++ Base/Lesson_3.py
@@ -0,0 +1,18 @@
+age = 10
+print(age)
+
+age = 15
+print('Значение переменной age равно', age)
+
+abc = 'Значение переменной'
+de = 'равно'
+print(abc, 'age', de, age)
+
+bul1 = True
+bul2 = False
+print(abc, 'bul1', de, bul1)  # булиевы
+print(abc, 'bul2', de, bul2)  # булиевы
+
+...
+12345
+...
