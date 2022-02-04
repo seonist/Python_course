18ee69406d133a6d0848033d9440d390e3bb9572
agrec <agrechushnikov@gmail.com>
2022-02-04 23:05:51 +0300

    Commit

---
+++ Base/Lesson_2.py
@@ -0,0 +1,14 @@
+age = 10
+print(age)
+
+age = 15
+print('Значение переменной age равно', age)
+
+abc = 'Значение переменной'
+de = 'равно'
+print(abc, 'age', de, age)
+
+bul1 = True
+bul2 = False
+print(abc, 'bul1', de, bul1)
+print(abc, 'bul2', de, bul2)
