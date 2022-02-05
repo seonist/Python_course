e92faf4950786bebb5e71fc98096fae70b20b134
agrec <agrechushnikov@gmail.com>
2022-02-05 18:16:28 +0300

    Добавлен 6-й урок

---
+++ Base/Lesson_6.py
@@ -0,0 +1,4 @@
+numeber1 = input('Введите первое число: ')
+numeber2 = input('Введите второе число: ')
+numeber3 = input('Введите третье число: ')
+print('Среднее арифметическое ваших чисел =', (int(numeber1) + int(numeber2) + int(numeber3)) / 3)
